b45d236bbb6be66756ca20e60917709f8d85cc42
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-11-21 00:48:51 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "https://mellifluous-puppy-efc100.netlify.app",
-    "http://localhost:5173"
+    "http://localhost:5000"
   ],
   credentials: true
 }));
